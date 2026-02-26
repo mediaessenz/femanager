e8e6eafadab8ce19570e3a719e9f33a985af4aab
Alexander Grein <alexander.grein@gmail.com>
2026-02-26 12:46:11 +0100

    Bump version to 14.0.0 and update TYPO3 constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,10 +13,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@in2code.de',
     'author_company' => 'in2code.de - Wir leben TYPO3',
     'state' => 'stable',
-    'version' => '13.2.1',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.4.99',
+            'typo3' => '13.0.0-14.3.99',
             'php' => '8.1.0-',
         ],
         'conflicts' => [],
